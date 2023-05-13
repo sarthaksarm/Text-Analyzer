b86d1711b1c264d55b1812556a52faa571ff5c46
Sarthak Mishra <sarmsarthak@gmail.com>
2023-05-13 13:01:20 -0700

    Updates

---
+++ precache-manifest.55a93b9bd6ee106b1b83e2a747e84068.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9059ca70ec9106e61de5",
+    "url": "/Text-Analyzer/static/css/main.8a8c5c57.chunk.css"
+  },
+  {
+    "revision": "9059ca70ec9106e61de5",
+    "url": "/Text-Analyzer/static/js/main.9059ca70.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/Text-Analyzer/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/Text-Analyzer/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "eae52549b17929ae48ab",
+    "url": "/Text-Analyzer/static/js/runtime~main.eae52549.js"
+  },
+  {
+    "revision": "a1f0d30bcc6c5f03d4c7d72c0b229cc1",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
